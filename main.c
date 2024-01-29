f405245d89ac6a917e60828cc9e119143af09f7b
ABarsen <116378189+ABarsen@users.noreply.github.com>
2024-01-29 10:12:58 +0300

    first commit

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main void()
+{
+    int  age= 40;
+    double grand = 100.000;
+    char grade = 'A';
+    char phrase [] = "manu kibe";   //string
+    printf("my name is %s\n",  phrase);
+    return 0;
+} 
+
